027fb65318a8468c3e90d1a4213b16edbdf17523
DESKTOP-377CJQD\Lovro <lovro.vrlec@gmail.com>
2023-03-19 22:10:43 +0100

    update error

--- src/main/java/org/example/moonrover/model/Direction.java
+++ src/main/java/org/example/moonrover/model/Direction.java
@@ -1,35 +1,33 @@
+/* (C)2023 */
 package org.example.moonrover.model;
 
-/**
- *  created by lovro.vrlec@gmail.com on Mar,2023
- */
+/** created by lovro.vrlec@gmail.com on Mar,2023 */
 public enum Direction {
-
     North,
     South,
     West,
     East;
 
     /**
-     * @param rotation wearher we are rotating left or right
+     * @param rotation weather we are rotating left or right
      * @return Direction after rotating left or right
      */
-    public Direction nextDirection(Rotation rotation){
-        if(this.equals(North) && Rotation.Left.equals(rotation)){
+    public Direction nextDirection(Rotation rotation) {
+        if (this.equals(North) && Rotation.Left.equals(rotation)) {
             return West;
-        } else if(this.equals(North) && Rotation.Right.equals(rotation)){
+        } else if (this.equals(North) && Rotation.Right.equals(rotation)) {
             return East;
-        } else if(this.equals(West) && Rotation.Left.equals(rotation)){
+        } else if (this.equals(West) && Rotation.Left.equals(rotation)) {
             return South;
-        } else if(this.equals(West) && Rotation.Right.equals(rotation)){
+        } else if (this.equals(West) && Rotation.Right.equals(rotation)) {
             return North;
-        } else if(this.equals(East) && Rotation.Left.equals(rotation)){
+        } else if (this.equals(East) && Rotation.Left.equals(rotation)) {
             return North;
-        } else if(this.equals(East) && Rotation.Right.equals(rotation)){
+        } else if (this.equals(East) && Rotation.Right.equals(rotation)) {
             return South;
-        } else if(this.equals(South) && Rotation.Left.equals(rotation)){
+        } else if (this.equals(South) && Rotation.Left.equals(rotation)) {
             return East;
-        } else if(this.equals(South) && Rotation.Right.equals(rotation)){
+        } else if (this.equals(South) && Rotation.Right.equals(rotation)) {
             return West;
         }
         return North;
--- src/main/java/org/example/moonrover/model/exception/RoverNotFoundException.java
+++ src/main/java/org/example/moonrover/model/exception/RoverNotFoundException.java
@@ -1,10 +1,12 @@
+/* (C)2023 */
 package org.example.moonrover.model.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/** created by lovro.vrlec@gmail.com on Mar,2023 */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class RoverNotFoundException extends Exception{
+public class RoverNotFoundException extends Exception {
 
     public RoverNotFoundException() {
         super("Rover Not Found");
